4bb60b78ad48d7927e49091e04fd5ffbb13c136f
ananya-arv <annyarvi@gmail.com>
2025-10-31 14:51:23 -0700

    basic infrastructure with auth

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// the routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
 // checking the health status of api
 app.get('/api/health', (req, res) => {
   res.status(200).json({ 
@@ -33,5 +37,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`The Server is running on port ${PORT}`);
+    console.log(`The Server is running on port ${PORT}`);
 });
