716b4f2a049b728a2ab69b723f3a961200964ae1
ananya-arv <annyarvi@gmail.com>
2025-11-14 11:53:30 -0800

    addition of logo css  with Joungseon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -155,7 +155,7 @@ function App() {
         <div className="auth-header">
             <div className="logo-circle ucla-logo-circle">
             <img
-              src={'${process.env.PUBLIC_URL}/campus-logo.jpg'}
+              src="https://logos-world.net/wp-content/uploads/2021/11/University-of-California-Los-Angeles-UCLA-Emblem.png"
               className="ucla-logo"
               />
           </div>
